c1aa7666e66c96848d312582cc96a19d083f7bb2
abrarcuet96 <133400726+abrarcuet96@users.noreply.github.com>
2023-11-08 10:40:35 +0600

    Create operation is done

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
-const app= express();
+const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
@@ -12,39 +12,48 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xyyby40.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    await client.connect();
-    // ----------------------------------------------
-    // CODE START FROM HERE
-    // ----------------------------------------------
+    try {
+        await client.connect();
+        // ----------------------------------------------
+        // CODE START FROM HERE
+        // ----------------------------------------------
+        const bookCollections = client.db("bookDB").collection("books");
+        // Create:
+        app.post('/addedBook', async (req, res) => {
+            const book = req.body;
+            const result = await bookCollections.insertOne(book);
+            res.send(result);
+        });
+        app.get('/addedBook', async(req,res)=>{
+            const result= await bookCollections.find().toArray();
+            res.send(result);
+        })
 
-
-
-    // ----------------------------------------------
-    // CODE ENDS HERE
-    // ----------------------------------------------
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-  }
+        // ----------------------------------------------
+        // CODE ENDS HERE
+        // ----------------------------------------------
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+    }
 }
 run().catch(console.dir);
 
 
 
-app.get('/', (req,res)=>{
+app.get('/', (req, res) => {
     res.send("Library is running");
 });
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`Library is running on port, ${port}`);
 });
