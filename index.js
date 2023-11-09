f09c54e5e6d4d5a5ea9030d55f64e8cf698c7df2
abrarcuet96 <133400726+abrarcuet96@users.noreply.github.com>
2023-11-09 19:01:05 +0600

    borrowed page database created

--- index.js
+++ index.js
@@ -90,7 +90,7 @@ async function run() {
         app.get('/borrowedPageBooks/:id', async(req,res)=>{
             const id=req.params.id;
             const query={_id: new ObjectId(id)};
-            const result = await borrowBookCollections.findOne(query).toArray();
+            const result = await borrowBookCollections.findOne(query);
             res.send(result);
         })
         // ----------------------------------------------
